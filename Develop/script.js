0ad280d0d5e7ba689bf6e0a49b8c4db56c05570c
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-09-09 22:50:28 -0700

    Added the rest of the code, changed the morn and after variables to hour-9,10,11 etc since momentjs is set to a 24 hour clock

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,77 @@
-var dateDisplayEl = $('#date-display');
+$(document).ready(function() {
+  
+    // assigning timeNow to moment format - day, month + date, year
+    let timeNow = moment().format('dddd, MMMM DD, YYYY');
+    $("#currentDay").html(timeNow);
 
-// call moment to display current date onto page
-function displayTime() {
-    var rightNow = moment().format('dddd, MMMM DD, YYYY');
-    dateDisplayEl.text(rightNow);
-  }
+  
+ 
 
   // save button
-$(".saveBtn").on("click", function() {
+$(".saveBtn").click(function() {
     // saves the text
-    var descr = $(this).siblings('.description').val();
+    let descr = $(this).siblings('.description').val();
     
     // specifies which id it is saved to -- asdfghjkl saved to 9AM
-    var hour = $(this).parent().attr("id");
+    let hours = $(this).parent().attr("id");
 
     // stores to the localStorage
-    localStorage.setItem(descr, hour);
+    localStorage.setItem(descr, hours);
+
+   
 } )
 
 // make the hour blocks synchronize with moment.js
+function nowHour () {
+  // declare the actual hour using momentjs
+  let actualHour = moment().hour();
+
+  // declare hour of the current block
+  // 'this' is set to time-block
+  $('.time-block').each(function() {
+    let scheduleHour = parseInt($(this).attr("id").split("-")[1]);
+ 
+  // use an if else statement to change the colors of the blocks
+  // use an each to target EACH function
+
+  if (scheduleHour < actualHour) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+  }
+  
+  else if (scheduleHour === actualHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+  }
+
+  else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+
+  })
+}
+  
 
 
-// use an if else statement to change the colors of the blocks
 
+nowHour();
 
-// interval to display time from momentjs
-  setInterval(displayTime);
+// grabbing value from local storage using .getItem
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+ 
+
+});
+
